f321850973f76d036b7e120d29c020bcc28b2600
ArturoCesar <arturitospartan117@hotmail.com>
2017-04-26 12:29:25 -0500

    Añadidos del pie de página

--- Documents/js/main.js
+++ Documents/js/main.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
 	$('.carousel').carousel();
 	$('.modal').modal();
 	$('.parallax').parallax();
+	$('.tap-target').tapTarget('open');
+    $('.tap-target').tapTarget('close'); 
 });
