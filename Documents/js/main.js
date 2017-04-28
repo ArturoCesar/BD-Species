622d61be2c5509b2d31230b2baf197425d90452e
ArturoCesar <arturitospartan117@hotmail.com>
2017-04-27 22:31:15 -0500

    Add files via upload

--- Documents/js/main.js
+++ Documents/js/main.js
@@ -3,5 +3,18 @@ $(document).ready(function(){
 	$('.modal').modal();
 	$('.parallax').parallax();
 	$('.tap-target').tapTarget('open');
-    $('.tap-target').tapTarget('close'); 
+    $('.tap-target').tapTarget('close');
+	$('.tooltipped').tooltip({delay: 50});	
+	function valida_envia(){
+		if(($("#contraseña")).value==""){
+			alert ("Por favor indique su nombre");
+			return 0;
+		}
+	};
+	function valida_envia2(){
+		if(($("#contraseña")).value==""){
+			alert ("Por favor indique su nombre");
+			return 0;
+		}
+	};
 });
