1be4f9c58fec7bde1f2220866d2766135ba4e611
ArturoCesar <arturitospartan117@hotmail.com>
2017-04-28 11:59:46 -0500

    Add files via upload

--- Templates/maindeprueba1.php
+++ Templates/maindeprueba1.php
@@ -9,15 +9,23 @@
 		echo $_POST["contra"]."<br/>";
 		$cuenta=$_POST["cuenta"];
 		$resultado = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario_id='".$cuenta."';");
-		//$resultado = mysqli_query($conexion, "SELECT usuario_nombre FROM usuarios;");
-		$consulta= mysqli_fetch_assoc($resultado);
-		$usuario=$consulta["usuario_id"];
-		if ($usuario==""){
+		$store= mysqli_store_result($conexion);
+		$campos= mysqli_field_count($conexion);
+		echo $campos."<br/>";
+		if ($campos==0){
+			echo "El usuario no existe";
+		}
+		else{
+			//$resultado = mysqli_query($conexion, "SELECT usuario_nombre FROM usuarios;");
+			$fila = mysqli_fetch_assoc($resultado);
+			$usuario=$fila["usuario_id"];
+			if ($usuario==""){
 				echo "el usuario no existe";
 			}
-		else {
-				echo $consulta["usuario_nombre"];
-		}
+			else {
+				echo $fila["usuario_nombre"];
+			}
 		//echo "Hubo un error: " . mysqli_error ( $mysqli );
+		}
 	}
 ?>
